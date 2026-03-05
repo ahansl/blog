84d8d25f7f6f98189f893c0ea1342400d605122c
Johannes Liegl <johannes.liegl@gmail.com>
2026-03-04 16:12:26 -0800

    Make blog base-path aware for /blog deployment

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,15 +6,17 @@ export async function GET(context) {
 		.filter((post) => !post.data.draft)
 		.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf());
 
+	const site = new URL(import.meta.env.BASE_URL, context.site);
+
 	return rss({
 		title: 'Blog',
 		description: 'A blog',
-		site: context.site,
+		site,
 		items: posts.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.pubDate,
 			description: post.data.description,
-			link: `/${post.id}`,
+			link: post.id,
 		})),
 	});
 }
